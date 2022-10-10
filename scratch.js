ece6949e1bf4af6deddf6076f4b7e0600cc790bd
Victor Delgado <valexdelgado@gmail.com>
2022-10-10 07:39:56 -0700

    getEpisodes() and getShowsByTerm() are finished

--- scratch.js
+++ scratch.js
@@ -17,7 +17,7 @@ async function displayTVshowObject(num){
 }
 
 
-console.log(displayTVshowObject(5))
+// console.log(displayTVshowObject(5))
 
 
 async function getBasicDataFromID(num){
@@ -33,22 +33,69 @@ async function getBasicDataFromID(num){
     }
     
 }
-console.log(getBasicDataFromID(5))
+// console.log(getBasicDataFromID(5))
+
 
 async function getEpisodes(num){
+    // solution code
+//     let response = await axios.get(`http://api.tvmaze.com/shows/${num}/episodes`)
+
+//     let episodes = response.data.map(episode => ({
+//         id: episode.id,
+//         name: episode.name,
+//         season: episode.season,
+//         number: episode.number,
+//     }))
+
+//     return episodes
+// }
+// end solution code
+    
+    // my code
+    
     const episodeData = await axios.get(`http://api.tvmaze.com/shows/${num}/episodes`)
-    const {season, name, number} = episodeData.data
+    results = []
     for (let item of episodeData.data){
-        console.log(item.season, item.name)
+        results.push([
+            {
+            'id': num,
+            'episode name': item.name,
+            'season': item.season,
+            'number': item.number 
+                    }])
     }
+    return results
 }
-getEpisodes(5)
+    // end my code
 
-// for (let i = 0; i<20; i++){
-//     console.log(getDatafromID(i))
-// }
+async function getShowsByTerm(query) {
+    // ADD: Remove placeholder & make request to TVMaze search shows API.
+    let termShows = await axios.get(`https://api.tvmaze.com/search/shows?q=${query}`)
+    results = []
+    for (let item of termShows.data){
+        results.push({
+            'id': item.show.id,
+            'name': item.show.name,
+            'summary': item.show.summary})
+    }
+    return results
 
-//async function getShowData(ID){
-    // const theData = await axios.get(`http://api.tvmaze.com/shows/${num}`)
-    // const { }
-// }
+    // return [
+    //     {
+    //     id: 1767,
+    //     name: "The Bletchley Circle",
+    //     summary:
+    //         `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
+    //             women with extraordinary skills that helped to end World War II.</p>
+    //         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
+    //             normal lives, modestly setting aside the part they played in 
+    //             producing crucial intelligence, which helped the Allies to victory 
+    //             and shortened the war. When Susan discovers a hidden code behind an
+    //             unsolved murder she is met by skepticism from the police. She 
+    //             quickly realises she can only begin to crack the murders and bring
+    //             the culprit to justice with her former friends.</p>`,
+    //     image:
+    //         "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+    //     }
+    // ]
+    }
